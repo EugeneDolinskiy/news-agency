22f80c2568afea02fa9d7cd2f13be7039dbd30f2
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-07 17:34:04 +0200

    create Redactor model

--- news_management/models.py
+++ news_management/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=False, blank=False)
+
+    class Meta:
+        ordering = ("-years_of_experience", )
+
+    def __str__(self):
+        return self.username
