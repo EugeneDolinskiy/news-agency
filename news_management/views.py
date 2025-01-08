3d82c281af5dee06c23c41c4614acea12e08150a
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-08 20:35:34 +0200

    create index view with dynamic content

--- news_management/views.py
+++ news_management/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from news_management.models import Redactor, Newspaper, Topic
+
+
+def index(request):
+    """View function for the home page of the News Agency site."""
+
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "news_management/index.html", context=context)
