4e9925bac02cedc319e3a9a304b4f4cacb8154ce
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-15 16:25:42 +0200

    Create RedactorCreationForm in forms.py

---
+++ news_management/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from news_management.models import Redactor
+
+
+class RedactorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "username",
+            "years_of_experience",
+        )
