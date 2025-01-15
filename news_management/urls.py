c27dee27cb1e23a8ad0a2e220fc8ec6b7265ee0d
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-15 14:31:35 +0200

    add path to newspaper list in urls.py

--- news_management/urls.py
+++ news_management/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from .views import (
     index,
     TopicListView,
+    NewspaperListView,
 )
 
 urlpatterns = [
     path('', index, name='index'),
     path('topics/', TopicListView.as_view(), name='topic-list'),
+    path('newspapers/', NewspaperListView.as_view(), name='newspaper-list'),
 ]
-
 
 app_name = 'news_management'
