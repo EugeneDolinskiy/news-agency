ece171049b25e5895ea842560cb05ee784ab02be
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-15 14:00:56 +0200

    add path to topic list in urls.py

--- news_management/urls.py
+++ news_management/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     index,
+    TopicListView,
 )
 
 urlpatterns = [
     path('', index, name='index'),
+    path('topics/', TopicListView.as_view(), name='topic-list'),
 ]
 
 
