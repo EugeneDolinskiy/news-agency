a150d135b524af5e1997fa9e1ded77f48afadb0c
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-15 15:02:07 +0200

    add path to newspaper detail in urls.py

--- news_management/urls.py
+++ news_management/urls.py
@@ -4,6 +4,7 @@ from .views import (
     index,
     TopicListView,
     NewspaperListView,
+    NewspaperDetailView,
     RedactorListView,
 )
 
@@ -11,6 +12,11 @@ urlpatterns = [
     path('', index, name='index'),
     path('topics/', TopicListView.as_view(), name='topic-list'),
     path('newspapers/', NewspaperListView.as_view(), name='newspaper-list'),
+    path(
+        'newspapers/<int:pk>/',
+        NewspaperDetailView.as_view(),
+        name='newspaper-detail'
+    ),
     path('redactors/', RedactorListView.as_view(), name='redactor-list'),
 ]
 
