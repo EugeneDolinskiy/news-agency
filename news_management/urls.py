c7b790fb8cbcf898f72c9b2ec5a99ecec8e812b4
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-01-16 16:39:35 +0200

    add path to topic creation page

--- news_management/urls.py
+++ news_management/urls.py
@@ -8,11 +8,13 @@ from .views import (
     RedactorListView,
     RedactorDetailView,
     RedactorCreateView,
+    TopicCreateView,
 )
 
 urlpatterns = [
     path('', index, name='index'),
     path('topics/', TopicListView.as_view(), name='topic-list'),
+    path('topic/create/', TopicCreateView.as_view(), name='topic-create'),
     path('newspapers/', NewspaperListView.as_view(), name='newspaper-list'),
     path(
         'newspapers/<int:pk>/',
@@ -21,7 +23,11 @@ urlpatterns = [
     ),
     path('redactors/', RedactorListView.as_view(), name='redactor-list'),
     path('redactors/create/', RedactorCreateView.as_view(), name='redactor-create'),
-    path('redactors/<int:pk>/', RedactorDetailView.as_view(), name='redactor-detail'),
+    path(
+        'redactors/<int:pk>/',
+        RedactorDetailView.as_view(),
+        name='redactor-detail'
+    ),
 ]
 
 app_name = 'news_management'
