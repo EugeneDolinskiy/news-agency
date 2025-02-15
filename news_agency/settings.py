8e3565f38f3ffe0efff3e920cf1484aa3b5b8be5
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-02-15 22:37:18 +0200

    remove all template files and implement good-look home page

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -65,7 +65,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'news_management.context_processors.cfg_assets_root',
             ],
         },
     },
