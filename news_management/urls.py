8b86808524ada9ac241ac7ee2fa32933feacc980
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-02-19 22:40:10 +0200

    use absolute imports instead of relative imports

--- news_management/urls.py
+++ news_management/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import (
+from news_management.views import (
     index,
     TopicListView,
     TopicCreateView,
--- news_management/views.py
+++ news_management/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from news_management.models import Redactor, Newspaper, Topic
-from .forms import (
+from news_management.forms import (
     RedactorCreateForm,
     NewspaperForm,
     NewspaperTitleSearchForm,
